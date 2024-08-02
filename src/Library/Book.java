f26c492e9589d1a541ec597b7dd09dc10c9c5bf3
Gabriel <gabrielbtegani@gmail.com>
2024-08-02 15:41:46 -0300

    adding some fixes and methods

--- src/Library/Book.java
+++ src/Library/Book.java
@@ -4,38 +4,94 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Book {
-   protected String author;
-   protected String title;
-   protected String publishingCompany;
-   protected LocalDate publicationYear;
+   private Integer isbn;
+   private String author;
+   private String title;
+   private String publishingCompany;
+   private LocalDate publicationYear;
    private LiteraryGenre literaryGenre;
+   private Double price;
+   private Integer quantity;
    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
    protected Book() {
    }
 
-   protected Book(String author, String title, String publishingCompany, LocalDate publicationYear, LiteraryGenre literaryGenre) {
+   protected Book(
+           Integer isbn, String author, String title, String publishingCompany, LocalDate publicationYear,
+           LiteraryGenre literaryGenre, Double price, Integer quantity
+   ) {
+      this.isbn = isbn;
       this.author = author;
       this.title = title;
       this.publishingCompany = publishingCompany;
       this.publicationYear = publicationYear;
       this.literaryGenre = literaryGenre;
+      this.price = price;
+      this.quantity = quantity;
+   }
+
+   public final Integer getIsbn() {
+      return isbn;
+   }
+
+   public final void setIsbn(Integer isbn) {
+      this.isbn = isbn;
    }
 
    public final String getAuthor() {
       return author;
    }
 
+   public final void setAuthor(String author) {
+      this.author = author;
+   }
+
    public final String getTitle() {
       return title;
    }
 
-   public final String getPublishingCompany() {
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public String getPublishingCompany() {
       return publishingCompany;
    }
 
-   public final String getPublicationYear() {
-      return this.publicationYear.format(this.formatter);
+   public void setPublishingCompany(String publishingCompany) {
+      this.publishingCompany = publishingCompany;
    }
 
+   public LocalDate getPublicationYear() {
+      return publicationYear;
+   }
+
+   public void setPublicationYear(LocalDate publicationYear) {
+      this.publicationYear = publicationYear;
+   }
+
+   public Library.LiteraryGenre getLiteraryGenre() {
+      return literaryGenre;
+   }
+
+   public void setLiteraryGenre(Library.LiteraryGenre literaryGenre) {
+      this.literaryGenre = literaryGenre;
+   }
+
+   public Double getPrice() {
+      return price;
+   }
+
+   public void setPrice(Double price) {
+      this.price = price;
+   }
+
+   public Integer getQuantity() {
+      return quantity;
+   }
+
+   public void setQuantity(Integer quantity) {
+      this.quantity = quantity;
+   }
 }
--- src/Library/Library.java
+++ src/Library/Library.java
@@ -2,26 +2,51 @@ package Library;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class Library {
    private final List<Book> booksList = new ArrayList<>();
-   private Double priceBook;
-   private int bookQuantity;
 
    public Library() {
    }
 
-   public void addBook(String author, String title, String publishingCompany, LocalDate publicationYear, LiteraryGenre literaryGenre) {
-      this.booksList.add(new Book(author, title, publishingCompany, publicationYear, literaryGenre));
+   public List<Book> getBooksList() {
+      return booksList;
+   }
+
+   public void addBook(
+           Integer isbn, String author, String title, String publishingCompany, LocalDate publicationYear,
+           LiteraryGenre literaryGenre, Double price, Integer quantity
+   ) {
+         Book book = new Book(
+                 isbn, author, title, publishingCompany, publicationYear, literaryGenre, price, quantity
+         );
+         if(this.booksList.contains(book)) {
+            Book bookExists = (Book) this.booksList.stream().filter(book1 -> book1 == book);
+            System.out.println("You already have this book in the library, one more will be added to your quantity");
+            bookExists.quantity++;
+         } else {
+            this.booksList.add(book);
+         }
    }
 
    public void removeBook(Book book) {
-      if(book == null) {
+      if(!this.booksList.contains(book)) {
          throw new LibraryException("This book doesn't exists in Library to remove");
       } else {
          this.booksList.remove(book);
       }
+   }
+
+   public void editAuthorName(Book book, String authorName) {
+      if(!this.booksList.contains(book)) {
+         throw new LibraryException("This book doesn't exists in Library to edit");
+      } else {
+         Book bookExists = (Book) this.booksList.stream().filter(book1 -> book1 == book);
+         bookExists.
+      }
+
    }
 
 }
